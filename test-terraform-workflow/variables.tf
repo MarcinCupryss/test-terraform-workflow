1d29ee39f92ed3c64397392ad2d37b97f3510799
Marcin Cupryś <162025701+MarcinCuprys13@users.noreply.github.com>
2024-05-06 18:56:02 +0200

    Variables type update

--- test-terraform-workflow/variables.tf
+++ test-terraform-workflow/variables.tf
@@ -1,9 +1,11 @@
 variable "message" {
-    description = "Script message"
-    default = "Hello world"
+    description     = "Script message"
+    type            = string
+    default         = "Hello world"
 }
 
 variable "file_path" {
-    description = "Path"
-    default =  "hello.js"
+    description     = "Path"
+    type            = string
+    default         =  "hello.js"
 }
